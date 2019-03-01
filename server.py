316d85c1ab136d10ad3c2677f3130f723e9575e0
RovelMan <jotdeepgahla@hotmail.com>
2019-02-28 18:40:43 -0800

    updated server class

--- server.py
+++ server.py
@@ -1,25 +1,10 @@
 import sys, socket, threading, pickle, time, random
 
 serverPorts = {'x': 7100, 'y': 7200, 'z': 7300}
-
+serverState = {'follower': 0, 'candidate': 1, 'leader': 2}
 
 class Server(object):
 
-  def __init__(self, id):
-    self.id = id
-
-  def startElection(self):
-    dkeys = serverPorts.keys()
-    for d in dkeys:
-      if (d != self.id):
-        s = socket.socket()
-        print("Sending stop message to " + str(serverPorts[d]))
-        s.connect(("127.0.0.1", serverPorts[d]))
-        s.send("stop")
-        s.close()
-    print("election over")
-
-class ListeningSocket(object):
   def __init__(self, id):
     if ((id != 'x') and (id != 'y') and (id != 'z')):
 		  print("Error! Invalid Server ID \nHas to be x, y or z \nQuitting...")
@@ -27,64 +12,67 @@ class ListeningSocket(object):
     self.id = id
     self.host = "127.0.0.1"
     self.port = serverPorts[id]
-    self.message = False
-    self.listeningPort = socket.socket()
-    self.listeningPort.bind((self.host, self.port))
-    self.listeningPort.listen(5)
+    self.message = None
+    self.currentState = serverState['follower']
     print("Setup for Server" + self.id.upper() + " done!")
-    self.run()
+    socketThread = threading.Thread(target=self.setupListeningSocket, args=(self.host, self.port))
+    timerThread = threading.Thread(target=self.setupTimer, args=(10,))
+    socketThread.daemon, timerThread.daemon = True, True
+    socketThread.start()
+    timerThread.start()
+    quitTerminal = ''
+    while quitTerminal != 'q': 
+      quitTerminal = raw_input("Write q to quit: ")
+      if quitTerminal != 'q':
+        print("Wrong input. Try Again!")
+      else:
+        print("Quitting")
 
-  def run(self):
-    connections = HandleIncomingMessages(self.listeningPort)
-    print("Running")
-    wait = raw_input("What next: ")
-    print(wait)
-
-class HandleIncomingMessages(object):
-  def __init__(self, listeningChannel):
-    thread = threading.Thread(target=self.run(listeningChannel), args=())
-    thread.daemon = True
-    thread.start()
-
-  def run(self, listeningChannel):
-    print("ready")
+  def setupListeningSocket(self, host, port):
+    listeningPort = socket.socket()
+    listeningPort.bind((host, port))
+    listeningPort.listen(5)
     while True:
-      t = Timeout(10)
-      conn, addr = listeningChannel.accept()
+      conn, addr = listeningPort.accept()
+      print('4')
       data = conn.recv(1024).decode()
-      print("Message recieved:", data)
-      if (data=="stop"):
-          t.setMessage(data)
-          break
+      print("Message recieved: " + data)
+      if (data == "STOP"):
+        self.message = "STOP"
+        break
       conn.close()
 
-class Timeout(object):
-  def __init__(self, interval=1):
-    self.interval = interval
-    self.message = None
-    thread = threading.Thread(target=self.run, args=())
-    thread.daemon = True
-    thread.start()
-
-  def setMessage(self, message):
-    self.message = message
-
-  def run(self):
-    currentInterval = self.interval
+  def setupTimer(self, interval=1):
+    currentInterval = interval
     while True:
       time.sleep(1)
-      if self.message == "stop":
+      if self.message == "STOP":
         print("Message recieved! Timer stopped")
         return
-      if currentInterval == 0:
-        s = Server(sys.argv[1])
-        s.startElection()
-        # print('Timer resetting')
-        # currentInterval = self.interval
+      if currentInterval == 0 and self.message == None:
+        self.startElection()
+        # self.currentState = Candidate()
+        # self.currentState.startElection()
         return
       else:
         print('Timer: ' + str(currentInterval) + ' seconds left')
         currentInterval -= 1
 
+  def startElection(self):
+    dkeys = serverPorts.keys()
+    for d in dkeys:
+      if (d != self.id):
+        try:
+          s = socket.socket()
+          print("Sending STOP message to " + str(serverPorts[d]))
+          s.connect(("127.0.0.1", serverPorts[d]))
+          s.send("STOP")
+          s.close()
+        except:
+          print("Server" + d.upper() + " is down!")
+          continue
+    print("Election Over")
+  
+
 if __name__ == '__main__':
-  server = ListeningSocket(sys.argv[1])
+  server = Server(sys.argv[1])
