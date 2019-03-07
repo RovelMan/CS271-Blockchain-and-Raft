3e05df2c4be51ec537fbecec0d855c8c2bf1eb65
RovelMan <jotdeepgahla@hotmail.com>
2019-03-06 18:19:51 -0800

    added functionality to make a blockchain

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import sys, socket, threading, pickle, time, random
+from bitcoinClasses import printBlock, Block
 
 serverPorts = {'x': 7100, 'y': 7200, 'z': 7300}
 serverState = {'follower': 0, 'candidate': 1, 'leader': 2}
@@ -9,6 +10,10 @@ class Server(object):
     if ((id != 'x') and (id != 'y') and (id != 'z')):
 		  print("Error! Invalid Server ID \nHas to be x, y or z \nQuitting...")
 		  return
+
+    self.blockchain = []
+    self.tempTxns = []
+
     self.id = id
     self.host = "127.0.0.1"
     self.port = serverPorts[id]
@@ -20,13 +25,20 @@ class Server(object):
     socketThread.daemon, timerThread.daemon = True, True
     socketThread.start()
     timerThread.start()
-    quitTerminal = ''
-    while quitTerminal != 'q': 
-      quitTerminal = raw_input("Write q to quit: ")
-      if quitTerminal != 'q':
-        print("Wrong input. Try Again!")
-      else:
+    command = ''
+    while command != 'q': 
+      print("Commands:")
+      print("\tSee blockchain: b")
+      print("\tQuit: q")
+      command = raw_input("Enter command: ")
+      if command == 'q':
         print("Quitting")
+      elif command == 'b':
+        print("Printing blockchain...")
+        for block in self.blockchain:
+          printBlock(block)
+      else:
+        print("Invalid command! Try again...")
 
   def setupListeningSocket(self, host, port):
     listeningPort = socket.socket()
@@ -34,11 +46,17 @@ class Server(object):
     listeningPort.listen(5)
     while True:
       conn, addr = listeningPort.accept()
-      data = conn.recv(1024).decode()
+      data = conn.recv(1024)
       print("Message recieved: " + data)
       if (data == "STOP"):
         self.message = "STOP"
         break
+      trans = pickle.loads(data)
+      print("Transaction received!", trans)
+      self.tempTxns.append(trans)
+      if len(self.tempTxns) == 2:
+        self.addToBlockchain(self.tempTxns)
+        self.tempTxns = []
       conn.close()
 
   def setupTimer(self, interval=1):
@@ -71,7 +89,15 @@ class Server(object):
           print("Server" + d.upper() + " is down!")
           continue
     print("Election Over")
-  
+
+  def addToBlockchain(self, txns):
+    block = Block(1, txns)
+    block.hash_block()
+    if len(self.blockchain) == 0:
+      block.hash_prev_block(None)
+    else:
+      block.hash_prev_block(self.blockchain[len(self.blockchain)-1])
+    self.blockchain.append(block)
 
 if __name__ == '__main__':
   server = Server(sys.argv[1])
